166d47e301cc32669483ceb7e1e3942fa7f0dd2a
GeethaSamminga <geethasamminga@gmail.com>
2024-09-09 15:09:22 +0530

    add

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const User = require('../models/users');
 const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const Tokengenerator = require('../utils/features');
 
