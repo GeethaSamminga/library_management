4ef5ba71a7971ae22df94c02cefb890ba0947f18
GeethaSamminga <geethasamminga@gmail.com>
2024-09-24 12:00:54 +0530

    changes in cors

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ const cors = require('cors');
 
 dotenv.config({ path: "./data/config.env" });
 
-
 connectDB();
 
-// Enable CORS
+// Enable CORS and handle preflight requests
 app.use(cors({
     origin: 'http://localhost:5173', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
 }));
+
+// Handle preflight (OPTIONS) requests globally
+app.options('*', cors());
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
